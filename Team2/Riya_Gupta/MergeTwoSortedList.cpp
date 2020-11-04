0bdcfba5217911876fb3b4664493da41530d12d5
gupta-riya <61061825+gupta-riya@users.noreply.github.com>
2020-11-04 18:29:08 +0530

    Update MergeTwoSortedList.cpp

--- Team2/Riya_Gupta/MergeTwoSortedList.cpp
+++ Team2/Riya_Gupta/MergeTwoSortedList.cpp
@@ -1,59 +1,61 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         
-        if(l1==null)
+        if(l1==NULL)
             return l2;
-        if(l2==null)
+        if(l2==NULL)
             return l1;
         
-        ListNode temp1 = l1 ; 
-        ListNode temp2 = l2;
-        ListNode result = new ListNode(0);
-        ListNode curr = result ; 
+        ListNode *temp1 = l1 ; 
+        ListNode *temp2 = l2;
+        ListNode *result = new ListNode(0);
+        ListNode *curr = result ; 
        
         
-        while(temp1!=null && temp2!=null)
+        while(temp1!=NULL && temp2!=NULL)
         {
-            if(temp1.val>=temp2.val)
+            if(temp1->val>=temp2->val)
             {
-                curr.next = temp2;
-                curr = curr.next;
-                 temp2 = temp2.next;
+                curr->next = temp2;
+                curr = curr->next;
+                 temp2 = temp2->next;
             }
             else
             {
-                 curr.next = temp1;
-                 curr = curr.next;
-                  temp1 = temp1.next;
+                 curr->next = temp1;
+                 curr = curr->next;
+                  temp1 = temp1->next;
                        
             }
         }
         
-        while(temp1!=null)
+        while(temp1!=NULL)
         {
-            curr.next = temp1 ; 
-            temp1 = temp1.next;
-            curr = curr.next;
+            curr->next = temp1 ; 
+            temp1 = temp1->next;
+          
+            curr = curr->next;
           
         }
-        while(temp2!=null)
+        while(temp2!=NULL)
         {
-            curr.next = temp2 ; 
-            temp2 = temp2.next;
-            curr = curr.next;
+            curr->next = temp2 ; 
+            temp2 = temp2->next;
+            curr = curr->next;
            
         }
        
-       return result.next; 
+       return result->next; 
     }
-}
+};
